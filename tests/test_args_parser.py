80ae13d47b86a85c087bc5627003019a582cb84a
Rinat Sabitov <rsabitov@cloudlinux.com>
2019-07-01 14:06:21 +0300

    Hostname of the asset is now meaningfull
    Signed-off-by: Rinat Sabitov <rsabitov@cloudlinux.com>

--- tests/test_args_parser.py
+++ tests/test_args_parser.py
@@ -1,3 +1,4 @@
+import mock
 
 import kcare_qualys
 
@@ -8,3 +9,17 @@ def test_configfile(tmpdir):
     parser = kcare_qualys.parse_args(["-c", configfile, "patch"])
     parser = kcare_qualys.parse_args(["-c", configfile, "ignore"])
     assert parser.configfile == configfile
+
+
+@mock.patch('kcare_qualys.setup_logging')
+@mock.patch('kcare_qualys.configparser')
+@mock.patch('kcare_qualys.qualysapi')
+@mock.patch('kcare_qualys.patch')
+def test_main(mock_patch, mock_qualys, mock_config, mock_logging):
+
+    mock_config.ConfigParser().has_option.return_value = True
+    mock_config.ConfigParser().get.return_value = 'test'
+    with mock.patch('kcare_qualys.sys.argv', ['kcare_qualys', 'patch']):
+        kcare_qualys.main()
+        mock_patch.assert_called_once()
+
